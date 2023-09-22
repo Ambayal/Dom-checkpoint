d58bb4bc12596c0c5ff7614cf30a4be7d21ec2b3
Ambayal <ambocoum@gmail.com>
2023-09-22 18:12:04 +0000

    first commit

--- src/data.js
+++ src/data.js
@@ -1,30 +1,30 @@
 let shopItemsData = [
-    {
-    id:"fdsgjsk",
-    name:"Casual Shirt",
+  {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
     price: 45,
-    desc:"lorem ipsum dolor sit amet, consectetur adip",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-1.jpg",
-}, 
-{
-    id:"degjfhejhfehfh",
-    name:"Office Shirt",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Office Shirt",
     price: 100,
-    desc:"lorem ipsum dolor sit amet, consectetur adip",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-2.jpg",
-}, 
-{     
-    id:"cdgfegufg",
-    name:"T Shirt",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "T Shirt",
     price: 25,
-    desc:"lorem ipsum dolor sit amet, consectetur adip",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-3.jpg",
-},
-{
-    id:"gdegfkkdlf",
-    name:"Mens Suit",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Mens Suit",
     price: 300,
-    desc:"lorem ipsum dolor sit amet, consectetur adip",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/img-4.jpg",
-},
+  },
 ];
